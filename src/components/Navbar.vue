a776a19af47445479226fe1d6cead8ad03da6c0e
marian <marian@cookandcode.org>
2023-10-25 00:22:50 +0200

    implement submenu + update navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,3 +1,7 @@
+<script setup>
+    import Submenu from './Submenu.vue';
+</script>
+
 <template>
     <div class="w-full">
         <div class="flex items-center justify-between">
@@ -17,9 +21,9 @@
                         style="width:40px" alt="Facebook Icon"></a>
             </div>
             <div class="block lg:hidden p-4 mr-4">
-                <div class="flex items-center border-black hover:text-grey hover:border-grey" v-on:click="toggleMenu()">
+                <div class="flex items-center border-black hover:text-grey hover:border-grey" >
                     <div
-                        class="js-burgermenu w-9 h-7 relative my-12 mx-auto transform rotate-0 transition ease-in-out duration-500 cursor-pointer">
+                        class="js-burgermenu w-9 h-7 relative my-12 mx-auto transform rotate-0 transition ease-in-out duration-500 cursor-pointer" v-on:click="toggleMenu()">
                         <span
                             class="upper block absolute h-[3px] w-full bg-black rounded opacity-100 right-0 transform rotate-0 transition ease-in-out duration-250 top-0"></span>
                         <span
@@ -30,53 +34,101 @@
                 </div>
             </div>
         </div>
-        <div :class="isActive ? 'opacity-90' : 'opacity-0'" :style="{pointerEvents: isClickable}"
+        <div :class="isActive ? 'opacity-95' : 'opacity-0'" :style="{pointerEvents: isClickable}"
             class="transition-opacity duration-500 ease-in-out lg:opacity-100">
-            <ul class="p-4 text-xl bg-ascBlue text-white fixed top-0 left-0 w-3/4 lg:static lg:flex lg:w-full lg:p-0"
-                v-on:click="toggleMenu()">
+            <ul class="p-4 pt-8 text-3xl lg:text-xl bg-ascBlue text-white text-center w-full absolute top-0 left-0 lg:static lg:flex lg:p-0 lg:pl-4 lg:pt-0 zoomable">
                 <li :class="isMobileView ? 'block' : 'hidden'">
-                    <RouterLink to="/" class="router-link" :class="isCurrentPath('/') ? 'nav-active-link' : ''">Home</RouterLink>
+                    <RouterLink to="/" class="router-link" :class="{ 'active': activeNavItem === 'home' }" v-on:click="toggleMenu()">Home</RouterLink>
+                </li>
+                <li @mouseenter="submenuIsOpenTeam1 = true" @mouseleave="submenuIsOpenTeam1 = false">
+                    <RouterLink to="/erste-mannschaft" class="router-link" :class="{ 'active': activeNavItem === 'erste-mannschaft' }" v-on:click="toggleMenu()">1. Mannschaft</RouterLink>
+                </li>
+                <li @mouseenter="submenuIsOpenTeam2 = true" @mouseleave="submenuIsOpenTeam2 = false">
+                    <RouterLink to="/zweite-mannschaft" class="router-link" :class="{ 'active': activeNavItem === 'zweite-mannschaft' }" v-on:click="toggleMenu()">2. Mannschaft</RouterLink>
                 </li>
                 <li>
-                    <RouterLink to="/erste-mannschaft" class="router-link" :class="isCurrentPath('/erste-mannschaft') ? 'nav-active-link' : ''">1. Mannschaft</RouterLink>
+                    <RouterLink to="/futsal" class="router-link" :class="{ 'active': activeNavItem === 'futsal' }" v-on:click="toggleMenu()">Futsal</RouterLink>
                 </li>
                 <li>
-                    <RouterLink to="/zweite-mannschaft" class="router-link" :class="isCurrentPath('/zweite-mannschaft') ? 'nav-active-link' : ''">2. Mannschaft</RouterLink>
+                    <RouterLink to="/vorstandschaft" class="router-link" :class="{ 'active': activeNavItem === 'board' }" v-on:click="toggleMenu()">Vorstandschaft</RouterLink>
                 </li>
                 <li>
-                    <RouterLink to="/futsal" class="router-link" :class="isCurrentPath('/futsal') ? 'nav-active-link' : ''">Futsal</RouterLink>
+                    <RouterLink to="/sponsoren" class="router-link" :class="{ 'active': activeNavItem === 'sponsors' }" v-on:click="toggleMenu()">Sponsoren</RouterLink>
                 </li>
                 <li>
-                    <RouterLink to="/vorstandschaft" class="router-link" :class="isCurrentPath('/vorstandschaft') ? 'nav-active-link' : ''">Vorstandschaft</RouterLink>
+                    <RouterLink to="/geschichte" class="router-link" :class="{ 'active': activeNavItem === 'history' }" v-on:click="toggleMenu()">Geschichte</RouterLink>
                 </li>
-                <li>
-                    <RouterLink to="/sponsoren" class="router-link" :class="isCurrentPath('/sponsoren') ? 'nav-active-link' : ''">Sponsoren</RouterLink>
-                </li>
-                <li>
-                    <RouterLink to="/geschichte" class="router-link" :class="isCurrentPath('/geschichte') ? 'nav-active-link' : ''">Geschichte</RouterLink>
-                </li>
+                <div class="block lg:hidden p-4 mr-4">
+                    <div class="flex items-center border-black hover:text-grey hover:border-grey">
+                        <div
+                            class="w-14 h-10 relative my-12 mx-auto transform rotate-0 transition ease-in-out duration-500 cursor-pointer" v-on:click="toggleMenu()">
+                            <span
+                                class="open upper block absolute h-[3px] w-full bg-white rounded opacity-100 right-0 transform rotate-0 transition ease-in-out duration-250 top-0"></span>
+                            <span
+                                class="open middle block absolute h-[3px] w-full bg-white rounded opacity-100 right-0 transform rotate-0 ease-in-out duration-250 top-3"></span>
+                            <span
+                                class="open lower block absolute h-[3px] w-full bg-white rounded opacity-100 right-0 transform rotate-0 transition ease-in-out duration-250 top-6"></span>
+                        </div>
+                    </div>
+                </div>
             </ul>
+            <div v-if="!isMobileView" class="relative">
+                <Submenu v-if="activeNavItem === 'erste-mannschaft'" :isOpen="submenuIsOpenTeam1">
+                    <li><a href="#league-1">Liga & Tabelle</a></li>
+                    <li><a href="#team-1">Kader</a></li>
+                    <li><a href="#training-1">Trainingszeiten</a></li>
+                </Submenu>
+                <Submenu v-if="activeNavItem === 'zweite-mannschaft'" :isOpen="submenuIsOpenTeam2">
+                    <li><a href="#league-2">Liga & Tabelle</a></li>
+                    <li><a href="#team-2">Kader</a></li>
+                    <li><a href="#training-2">Trainingszeiten</a></li>
+                </Submenu>
+            </div>  
         </div>
     </div>
 </template>
 
 <script>
 export default {
+    components:{
+        Submenu
+    },
     data() {
         return {
             isActive: false,
-            isMobileView: false
+            isMobileView: false,
+            submenuIsOpenTeam1: false,
+            submenuIsOpenTeam2: false,
+            activeNavItem: null
         };
     },
+    watch: {
+    $route() {
+      this.updateActiveNavItem();
+    }
+  },
     methods: {
-        isCurrentPath(path) {
-            const pathArray = window.location.pathname.split('/');
-            const indexLastSlash = pathArray.length - 1;
-            const lastIndex = pathArray[indexLastSlash]
-            return "/"+lastIndex === path;
+        updateActiveNavItem() {
+            const path = this.$route.path;
+            if (path === '/') {
+                this.activeNavItem = 'home';
+            } else if (path === '/erste-mannschaft') {
+                this.activeNavItem = 'erste-mannschaft';
+            } else if (path === '/zweite-mannschaft') {
+                this.activeNavItem = 'zweite-mannschaft';
+            } else if (path === '/futsal') {
+                this.activeNavItem = 'futsal';
+            } else if (path === '/vorstandschaft') {
+                this.activeNavItem = 'board';
+            } else if (path === '/sponsoren') {
+                this.activeNavItem = 'sponsors';
+            } else if (path === '/geschichte') {
+                this.activeNavItem = 'history';
+            }
         },
 
         toggleMenu() {
+            console.log("toggleMenu() loaded")
             this.isActive = !this.isActive;
             this.showX();
         },
@@ -88,14 +140,15 @@ export default {
             for (const span of spans) {
                 span.classList.toggle('open');
             }
-        }
+        },
     },
     computed: {
         isClickable(){
+            // Mobile view navbar is just clickable if the menu is visible (opacity 90)
             return this.isMobileView ? this.isActive ? 'auto' : 'none'  :  'auto';
         }
     },
-    mounted() {
+    mounted() {  
         // Update isMobileView when the component is mounted
         this.isMobileView = window.innerWidth < 1024;
 
@@ -109,19 +162,19 @@ export default {
 
 <style>
 li {
-    padding: 1rem;
+    padding: 1rem 1.5rem;
+}
+
+.zoomable > li:hover {
+    transform: scale(1.2);
+    cursor: pointer;
 }
 
 .router-link{
     transition: color .3s ease-in-out, box-shadow .3s ease-in-out;
 }
 
-.router-link:hover{
-    color: #080F29; 
-    box-shadow: inset 200px 0 0 0 #fff;;
-}
-
-.nav-active-link{
+.active{
     text-decoration: underline;
 }
 
---
+++ src/components/Submenu.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="absolute left-0 top-0 border border-ascBlue lg:w-full opacity-90 bg-ascBlue" v-if="isOpen" @mouseenter="isOpen = true"
+        @mouseleave="isOpen = false">
+        <ul class="text-white p-4 text-xl lg:flex lg:w-full lg:p-0 lg:pl-4 subnav zoomable">
+            <slot></slot>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        isOpen: Boolean
+    }
+}
+</script>
+
+<style>
+.subnav li{
+    font-size: 1rem;
+}
+</style>
